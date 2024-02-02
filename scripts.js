4fe2ae6427fa286253e4fb4ffef63b9524f3ce20
juandiegorubio <juan.diego.rubio@estudiantat.upc.edu>
2024-02-02 22:52:23 +0100

    Refactor button design

--- scripts.js
+++ scripts.js
@@ -11,7 +11,12 @@ function createCardElement() {
     card.id = 'card' + (++globalThis.cardID).toString();
   
     // Add inner html
-    let card_content = '<div class="top"><input type="text" class="card-title" /><button class="delete-card" onClick="delete_card(this)"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg></button></div>'
+    let card_content = `<div class="top">
+        <input type="text" class="card-title" />
+          <button class="has-icon delete-card" onClick="delete_card(this)">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
+          </button>
+      </div>`
     card.innerHTML = card_content;
 
     // Make card draggable
@@ -50,4 +55,16 @@ function dropCard(ev) {
     let card_list = findAncestor(ev.target, 'list');
     card_list = document.getElementById(card_list.id).getElementsByClassName("cardList")[0];
     card_list.appendChild(card_to_add);
+}
+
+
+
+
+let inputs = document.getElementsByTagName('input');
+for (let input of inputs) {
+  input.addEventListener('keypress', (event) => {
+    if (event.key === "Enter") {
+      console.log("Stop input");
+    }
+  });
 }
